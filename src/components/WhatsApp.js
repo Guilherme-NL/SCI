0c3e419e0033e76c262447de9316ce0a6fc2802c
Guilherme-NL <guilherme.nunes.lopes@gmail.com>
2023-05-15 13:07:55 -0300

    fix: config whats app

--- src/components/WhatsApp.js
+++ src/components/WhatsApp.js
@@ -1,11 +1,11 @@
 import React from "react";
 import { FloatingWhatsApp } from "react-floating-whatsapp";
-import logo from "../images/logo-cut.png";
+import logo from "../images/file.enc";
 
 export default function WhatsApp() {
   return (
     <FloatingWhatsApp
-      phoneNumber="553135711370"
+      phoneNumber="553140420120"
       avatar={logo}
       statusMessage="on-line"
       chatMessage="Olá, tudo bem? Como posso te ajudar?"
